700d0c6ec1898ad7701936e9468d01f9a57ea9a8
tu nombre <carmencoboruizazuaga@gmail.com>
2023-02-26 01:47:43 +0100

    editar imagen

--- src/components/editar_curso.vue
+++ src/components/editar_curso.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { ref } from "vue"
+import { getStorage, ref as ref2, uploadBytes, getDownloadURL } from "firebase/storage";
+
 import {useRoute} from "vue-router";
 
 import { useFirestore } from 'vuefire'
@@ -16,12 +19,28 @@ function editar_curso() {
 
     if (categoria != "" && nombre != "" && horas != "" && categoria != "") {
         updateDoc(curso_bd, {
-            capital: categoria,
+            categoria: categoria,
             nombre: nombre,
             horas: horas,
-            // imagen: file.value.files[0].name 
+            imagen: file.value.files[0].name 
         });
     }
+}
+
+let file= ref("")
+//subir archivos
+function subir_archivo() {
+    const storage = getStorage();
+    const storageRef = ref2(storage, file.value.files[0].name);
+
+    // 'file' comes from the Blob or File API
+    uploadBytes(storageRef, file.value.files[0]).then((snapshot) => {
+    console.log('Uploaded a blob or file!');
+    getDownloadURL(storageRef)
+        .then((url) => {
+            console.log(url);
+        })
+    });
 }
 
 //título componente
@@ -50,9 +69,8 @@ defineProps({
       <input type="number" name="horas" id="horas" min="0"> <br>
 
       <label for="imagen">Imagen: </label>
-      <!-- <input type="file" name="" accept="image/*" ref="file" @change="subir_archivo"> <br> -->
+      <input type="file" name="" accept="image/*" ref="file" @change="subir_archivo"> <br>
 
-      <br>
       <input type="submit" @click="editar_curso" value="Editar curso"> 
 
   </fieldset>
